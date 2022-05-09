8c380d534b44f844ded219bbaecfea0f32d63795
Emrys Shevek <mpoggemann96@gmail.com>
2022-05-08 19:53:33 -0700

    removed unnecessary files

--- server/database/Message.js
+++
@@ -1,9 +0,0 @@
-
-class Message {
-    constructor (contents, senderID, groupID){
-        this.contents = contents
-        this.senderID = senderID
-        this.groupID = groupID
-        this.sentTime = Date.now()
-    }
-}
