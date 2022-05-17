f9c19829fca191b25b579f4431abdaabab13315e
ZOrangeBandit <awsomenim@gmail.com>
2022-05-17 15:03:49 -0700

    Delete clog

--- client/src/pages/NoPage.js
+++ client/src/pages/NoPage.js
@@ -13,23 +13,18 @@ const NoPage = ({validateUser = defaultProps.validateUser}) => {
     const hexadecimal = /^[0-9a-fA-F]+$/;
     const navigate = useNavigate();
     React.useEffect(async ()=>{
-        console.log(hexadecimal.test(window.location.pathname.split("/")[1]), window.location.pathname.split("/")[1])
         if(hexadecimal.test(window.location.pathname.split("/")[1]) || window.location.pathname.split("/")[1]){
             await validateUser(window.location.pathname.split("/")[1])
                 .then((res, rej)=>{
-                    console.log(res, rej);
                     if(rej){
-                        console.log("rej", rej)
                         navigate("/error404");
                     }
                     navigate("/chat");
                 })
                 .catch((err)=>{
-                    console.log("err", err);
                     navigate("/error404");
                 })
         } else {
-            console.log("else")
             navigate("/error404")
         }
     })
